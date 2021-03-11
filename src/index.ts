e2ba0ffb9f0514eea85152d21d3e6c43194aa56f
GiovanniCardamone <g.cardamone2@gmail.com>
2021-03-11 15:57:54 +0100

    added produces/consumes definition per routes

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,8 @@ interface StrictAnyRoute extends AnyRoute {
     description?: string
     security?: Security[]
     tags?: string[]
-
+    consumes?: string[]
+    produces?: string[]
     body?: JsonSchemaProperty
     querystring?: JsonSchemaProperty
     params?: ObjectJsonSchemaProperty['properties']
